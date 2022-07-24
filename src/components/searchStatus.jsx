e61f689fa44f6f09fc37fec64c93b709b8414c7f
Radko Ksenia <ksradko16@yandex.ru>
2022-07-24 17:43:51 +0300

    add pagination and prettier

--- src/components/searchStatus.jsx
+++ src/components/searchStatus.jsx
@@ -1,29 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const SearchStatus = ({ length }) => (
     <h2>
-        <span 
-        className={"badge " + (length === 0? 
-        "bg-danger": "bg-primary")}
+        <span
+            className={"badge " + (length === 0 ? "bg-danger" : "bg-primary")}
         >
             {getSearchStatusPhrase(length)}
         </span>
     </h2>
-)
+);
 
+SearchStatus.propTypes = {
+    length: PropTypes.number.isRequired
+};
 
 const getSearchStatusPhrase = (length) => {
     const phrase = "с тобой сегодня";
-    let num = length % 100;
-    let value = num % 10;
-    if (length === 0) return "Никто с тобой не тусанет!"
+    const num = length % 100;
+    const value = num % 10;
+    if (length === 0) return "Никто с тобой не тусанет!";
     if (num > 11 && num < 15) {
         return length + " человек тусанет " + phrase;
-    } 
+    }
     if (value > 1 && value < 5) {
         return length + " человека тусанут " + phrase;
-    } 
+    }
     return length + " человек тусанет " + phrase;
-}
+};
 
 export default SearchStatus;

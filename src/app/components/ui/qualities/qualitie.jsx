6d7befefba6453a78719a470e07cc05f81d4225f
Radko Ksenia <ksradko16@yandex.ru>
2022-10-24 10:57:26 +0300

    init sign up with email/password

--- src/app/components/ui/qualities/qualitie.jsx
+++ src/app/components/ui/qualities/qualitie.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useQualities } from "../../../hooks/useQualities";
 
-const Qualitie = ({ _id, color, name }) => {
+const Qualitie = ({ id }) => {
+    const { getQuality } = useQualities();
+    const { color, name } = getQuality(id);
     return (
-        <span key={_id} className={"badge m-1 bg-" + color}>
+        <span className={"badge m-1 bg-" + color}>
             {name}
         </span>
     );
 };
 
 Qualitie.propTypes = {
-    _id: PropTypes.string,
-    color: PropTypes.string,
-    name: PropTypes.string
+    id: PropTypes.string
 };
 
 export default Qualitie;
